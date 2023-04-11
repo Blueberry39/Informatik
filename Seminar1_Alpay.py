561fae39b7f2a45f22e32a253e59942c11c4c42b
Blueberry39 <124323600+Blueberry39@users.noreply.github.com>
2023-04-11 15:42:20 +0200

    Informatik Aufgaben

--- Seminar1_Alpay.py
+++ Seminar1_Alpay.py
@@ -3,3 +3,8 @@
 euro = float(input("Wieviel € sollen umgerechnet werden?:"))
 dollar = round(euro*1.09, 2)
 print("Der Wechselkurs beträgt", dollar, "$")
+
+def doppel():
+    x=int(input("Bitte geben Sie eine Zahl ein: "))
+    y=2*x
+    print("Das Doppelte Ihrer Zahl ist:",y)
